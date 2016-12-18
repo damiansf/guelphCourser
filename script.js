1ddef0989cf707008cfefc636a595e5f12228fab
yhuang133 <yhuang13@mail.uoguelph.ca>
2016-12-18 00:40:20 -0500

    Basic jquery that finds if text is in page, then alerts browers with message if it is found

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+//gets html in "body" class
+var htmlString = $('body').html().toString();
+
+//The indexOf() method returns the position of the first occurrence of a specified value in a string. This 
+//method returns -1 if the value to search for never occurs.
+var index = htmlString.indexOf("Sections");
+
+if (index != -1)
+{
+    alert("FOUND SECTIONS IN PAGE");
+}
+
+function storeData()
+{
+}
+
+function parse()
+{
+}
