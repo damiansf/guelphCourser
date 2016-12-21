d38d17cabe55f7e0e61b967efe031a9ed88c6100
yhuang133 <yhuang13@mail.uoguelph.ca>
2016-12-21 15:40:33 -0500

    finished linked list impleentation for storing courses (add, remove, get)

--- script.js
+++ script.js
@@ -1,61 +1,14 @@
-$(document).ready(function()
-{
-    //gets html in "body" class
-    var htmlString = $('body').html().toString(),
-
-    //The indexOf() method returns the position of the first occurrence of a specified value in a string. This 
-    //method returns -1 if the value to search for never occurs.
-        indexTitle = htmlString.indexOf("Section Selection Results"),
-        indexCourse = htmlString.indexOf("Section Name and Title");
-
-    if (indexTitle != -1 && indexCourse != -1)
-    {
-        parseAndStore();
-    }
-});
-
-function parseAndStore()
-{
-    $("#GROUP_Grp_WSS_COURSE_SECTIONS tr").each(function(index){
-        if(index !== 0) //skip the first since fields start at 1
-        {
-            var termText = $("#SEC_TERM_" + index).text(),
-            statText = $("#LIST_VAR2_" + index).text(),
-            nameText = $("#SEC_SHORT_TITLE_" + index).text(),
-            locationText = $("#SEC_LOCATION_" + index).text(),
-
-            meetingLec = $('div.meet.LEC', this).children(),
-            meetingText = "",
-
-            facultyText = $("#SEC_FACULTY_INFO_" + index).text(),
-            availText = $("#LIST_VAR3_" + index).text();
-
-            for(i = 0; i < meetingLec.length; i += 1)
-            {
-                meetingText += $(meetingLec[i]).text() + "\n";
-            }
-
-            console.log(termText+" "+statText+" "+nameText+" "+locationText+" "+facultyText+" "+availText + " ");
-            console.log(meetingText);
-        }
-            
-        
-    });
-}
-
 //course object with course information
-var courseObj = {
-    term:null,
-    stat:null,
-    name:null,
-    locat:null,
-    meetingInfo:null,
-    faculty:null,
-    avail:null,
-    credits:null,
-    academ:null
-
-};
+function courseObj(term, stat, name, locat, meetingInfo, faculty, avail)
+{
+    this.term = term;
+    this.stat = stat;
+    this.name = name;
+    this.locat = locat;
+    this.meetingInfo = meetingInfo;
+    this.faculty = faculty;
+    this.avail = avail;
+}
 
 //linked list node
 function Node(course)
@@ -72,74 +25,136 @@ function List()
 }
 
 //add to list alphabetically, returns added node
-List.prototype.insert = function(course)
+List.prototype.add = function(course)
 {
     var node = new Node(course),
-        currentNode = this.head;
+        curr = this.head,
+        prev = null;
 
-    //if empty list
-    if(currentNode == null)
+    while(curr && curr.course.name < node.course.name)
+    {
+        prev = curr;
+        curr = curr.next;
+    }
+
+    if(!prev)
     {
         this.head = node;
-        this.length += 1;
-
-        return node;
     }
-
-    //non-empty list case, insert in alphabetic course title order and more spots open
-    while(currentNode.next != null && currentNode.course.name < node.course.name &&  currentNode.course.avail < node.course.avail)
+    else
     {
-        currentNode = currentNode.next;
+        prev.next = node;
+    }
+    if(curr)
+    {
+        node.next = curr;
     }
 
-    currentNode.next = node;
-
-    this.length += 1;
-
+    this.size += 1;
     return node;
-}
+};
 
 //delete function, deletes at p, returns deleted node
-List.prototype.remove = function(position)
+List.prototype.remove = function(i)
 {
-    var currentNode = this.head,
-        size = this.size,
-        count = 0,
-        message = {failure: 'trying to delete non-existent node'};
-        lastNode = null,
-        currentNode = null,
-        deletedNode = null;
+    var curr = this.head;
+    var prev = null;
 
-    //bad position
-    if(position < 0 || position > size)
+    while(i > 0 && curr.next)
     {
-        throw new Error(message.failure);
+        i -= 1;
+        prev = curr;
+        curr = curr.next;
     }
-
-    //deleting head
-    if(position === 1)
+    if(i != 0)
     {
-        this.head = currentNode.next;
-        deletedNode = currentNode;
-        currentNode = null;
-        this.size -= 1;
-
-        return deletedNode;
+        throw new Error();
     }
-
-    //deleting anything else
-    while(count < position)
+    if(prev && curr.next)
     {
-        lastNode = currentNode;
-        currentNode = currentNode.next;
-        count += 1;
+        prev.next = curr.next;
     }
-
-    lastNode.next = currentNode.next;
-    deletedNode = currentNode;
-    currentNode = null;
     this.size -= 1;
+    return curr;
 
-    return deletedNode;
+};
 
+List.prototype.getNode = function(i)
+{
+    var curr = this.head;
+    if(i < 0 || i > this.size - 1)
+    {
+        throw new Error();
+    }
+    while(i > 0)
+    {
+        i -= 1;
+        curr = curr.next;
+    }
+
+    return curr;
+
+};
+
+function parseAndStore(allCourses)
+{
+    $("#GROUP_Grp_WSS_COURSE_SECTIONS tr").each(function(index){
+        if(index !== 0) //skip the first since fields start at 1
+        {
+            var termText = $("#SEC_TERM_" + index).text(),
+            statText = $("#LIST_VAR2_" + index).text(),
+            nameText = $("#SEC_SHORT_TITLE_" + index).text(),
+            locationText = $("#SEC_LOCATION_" + index).text(),
+
+            meetingLec = $('div.meet.LEC', this).children(),
+            meetingExam = $('div.meet.EXAM', this).children(),
+            meetingText = "",
+
+            facultyText = $("#SEC_FACULTY_INFO_" + index).text(),
+            availText = $("#LIST_VAR3_" + index).text();
+
+            for(i = 0; i < meetingLec.length; i += 1)
+            {
+                meetingText += $(meetingLec[i]).text() + "\n";
+            }
+
+            for(x = 0; x < meetingExam.length; x += 1)
+            {
+                meetingText += $(meetingExam[x]).text() + "\n";
+            }
+
+            var course = new courseObj(termText, statText, nameText, locationText, meetingText, facultyText, availText);
+            allCourses.add(course);
+        }
+    });
 }
+
+//main function, runs on page load
+$(document).ready(function()
+{
+    //gets html in "body" class
+    var htmlString = $('body').html().toString(),
+
+    //The indexOf() method returns the position of the first occurrence of a specified value in a string. This
+    //method returns -1 if the value to search for never occurs.
+    indexTitle = htmlString.indexOf("Section Selection Results"),
+    indexCourse = htmlString.indexOf("Section Name and Title");
+
+    var allCourses = new List();
+    if (indexTitle != -1 && indexCourse != -1)
+    {
+        parseAndStore(allCourses);
+
+        /** removes all courses inside it, tests remove function
+        while(allCourses.size)
+        {
+            allCourses.remove(allCourses.size-1);
+        }
+        //outputs all the courses in the list, tests the get function
+        for(k = 0; k < allCourses.size; k += 1)
+        {
+            console.log(allCourses.getNode(k).course.name + "\n");
+        }
+        **/
+    }
+});
